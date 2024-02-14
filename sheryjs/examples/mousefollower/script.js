74fd3821ec30f131b135814f3e73876392328090
Aayush Chouhan <aayushchouhan24@gmail.com>
2024-02-15 04:30:28 +0530

    fix

--- sheryjs/examples/mousefollower/script.js
+++ sheryjs/examples/mousefollower/script.js
@@ -1,11 +1,6 @@
-const pane = new tweakpane.Pane({title:'Debug Panel'})
 const parm = {
     skew: true,
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
 }
-
-pane.addBinding(parm,'skew')
-pane.addBinding(parm,'duration')
-
 Shery.mouseFollower(parm)
